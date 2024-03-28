68a4743e8b482cab7b1cb36c171a6bbf090d8372
Vaidehi Pujary <vaipujary@gmail.com>
2024-03-28 14:28:21 -0700

    PWM header update

--- Lab 4/src/pwm.h
+++ Lab 4/src/pwm.h
@@ -1,7 +1,8 @@
 #ifndef TIMER_H
-//hellp world
+// hellp world
 #define TIMER_H
 void initPWMTimer3();
 void initPWMTimer4();
 void changeDutyCycle(double dutyCycle1, double dutyCycle2);
+void changeDutyCycle(unsigned int OCRval);
 #endif
