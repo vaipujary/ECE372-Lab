e8eb4ce2ae4899d7a6c7517db58e9d058b32c54a
Vaidehi Pujary <vaipujary@gmail.com>
2024-04-18 14:14:14 -0700

    interrupt

--- Lab 5/src/main.cpp
+++ Lab 5/src/main.cpp
@@ -15,79 +15,87 @@
 #include "timer.h"
 #include "switch.h"
 #include "i2c.h"
-typedef enum {
+typedef enum
+{
   waitPress,
   debouncePress,
   waitRelease,
-  debounceRelease} buttonState;
-typedef enum {
-    LEDSMILLEY,LEDSAD
-}LEDFACES;
+  debounceRelease
+} buttonState;
+typedef enum
+{
+  LEDSMILLEY,
+  LEDSAD
+} LEDFACES;
 volatile int x;
 volatile int y;
 volatile int z;
 
 #define MPU_WHO_AM_I 0x68
 volatile buttonState myButtonState = waitPress;
-volatile LEDFACES LEDState =LEDSMILLEY;
-int ChirpOn=0;//chirp=0 no chirp,1 chirping
-int main(){
-    initI2C();
-    initPWMTimer3();
-    initSPI();
-    initTimer1();
-    initSwitchPD2();
-    Serial.begin(9600);
-    read_Data();
-    while(1) {
-      Serial.print("x: "+String(x)+"\n");
-      Serial.print("y: "+String(y)+"\n");
-      Serial.print("z: "+String(z)+"\n");
-      for (int i = 1000; i <= 4000; i++) {
-        changeFrequency(i);
-      }
-
-      switch (LEDState)
-      {
-      case LEDSMILLEY:
-        displaySmile();
-        alarmOff();
-        break;
-      case LEDSAD:
-        displayFrown();
-        break;
-      default:
-        break;
-      }
-      switch (myButtonState)
-      {
-      case waitPress:
-        Serial.println("waitPress");
-        delayMs(1000);
-        break;
-      case debouncePress:
-        Serial.println("debouncePress");
-        myButtonState = waitRelease;
-        break;
-      case waitRelease:
-        Serial.println("waitRelease");
-        break;
-      case debounceRelease:
-        Serial.println("debounceRelease");
-        myButtonState = waitPress;
-        break;
-      default:
-        break;
-      }
+volatile LEDFACES LEDState = LEDSMILLEY;
+int ChirpOn = 0; // chirp=0 no chirp,1 chirping
+int main()
+{
+  initI2C();
+  initPWMTimer3();
+  initSPI();
+  initTimer1();
+  initSwitchPD2();
+  Serial.begin(9600);
+  read_Data();
+  while (1)
+  {
+    Serial.print("x: " + String(x) + "\n");
+    Serial.print("y: " + String(y) + "\n");
+    Serial.print("z: " + String(z) + "\n");
+    for (int i = 1000; i <= 4000; i++)
+    {
+      changeFrequency(i);
     }
+
+    switch (LEDState)
+    {
+    case LEDSMILLEY:
+      displaySmile();
+      alarmOff();
+      break;
+    case LEDSAD:
+      displayFrown();
+      break;
+    default:
+      break;
+    }
+    switch (myButtonState)
+    {
+    case waitPress:
+      Serial.println("waitPress");
+      delayMs(1000);
+      break;
+    case debouncePress:
+      Serial.println("debouncePress");
+      myButtonState = waitRelease;
+      break;
+    case waitRelease:
+      Serial.println("waitRelease");
+      break;
+    case debounceRelease:
+      Serial.println("debounceRelease");
+      myButtonState = waitPress;
+      break;
+    default:
+      break;
+    }
+  }
 }
 
+ISR(PCINT0_vect)
+{
 
-ISR(INT2_vect) {
-  
-  if(x>10 && y>10 && z>10){
+  if (x > 10 && y > 10 && z > 10)
+  {
     alarmOn();
-    LEDState=LEDSAD;
+    LEDState = LEDSAD;
   }
   if (myButtonState == waitPress)
   {
@@ -106,8 +114,6 @@ ISR(INT2_vect) {
     //   flip = 1;
     // }
     myButtonState = debounceRelease;
-    LEDState=LEDSMILLEY;
+    LEDState = LEDSMILLEY;
   }
-
-
 }
