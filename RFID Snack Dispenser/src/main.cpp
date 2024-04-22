200c7a2a1d4e0db1b1dcdc2702674043ac195fde
Vaidehi Pujary <vaipujary@gmail.com>
2024-04-21 22:32:52 -0700

    state machine

--- RFID Snack Dispenser/src/main.cpp
+++ RFID Snack Dispenser/src/main.cpp
@@ -24,6 +24,16 @@ MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
 int rfidUID = 0;
 int UID_list[10];
 
+typedef enum
+{
+    waitPress,
+    debouncePress,
+    waitRelease,
+    debounceRelease
+} buttonState;
+
+volatile buttonState myButtonState = waitPress;
+
 // Main function
 int main(void)
 {
@@ -33,9 +43,60 @@ int main(void)
     initRFID();
     initSwitchPD0();
     rfidUID = readRFID();
+
+    while (1)
+    {
+
+        // Button state machine logic
+        switch (myButtonState)
+        {
+            ///////////////////////////////Press States/////////////////////////////////////////
+        case waitPress: // the "natural" state
+            // Do nothing, wait for button to be pressed
+            Serial.println(1);
+            break;
+
+        case debouncePress: // Debounce Press state, wait for swithc debounce state to end
+            Serial.println("debouncePress");
+            Serial.flush();
+            delayMs(1);
+            myButtonState = waitRelease;
+            break;
+
+        ///////////////////////////////Release States///////////////////////////////
+        case waitRelease: // waits for button to be released after pressed
+            Serial.println("waitRelease");
+            Serial.flush();
+            delayMs(1);
+
+            break;
+
+        case debounceRelease:
+            Serial.println("debounceRelease");
+            Serial.flush();
+            delayMs(1);
+            myButtonState = waitPress;
+            break;
+
+        default:
+            break;
+        }
+    }
 }
 
 // Interrupt Service Routine
 ISR(PCINT0_vect)
 {
+    Serial.println("Button pressed!");
+
+    if (myButtonState == waitPress)
+    {
+        Serial.println("waitPress --> debouncePress");
+        myButtonState = debouncePress;
+    }
+    else if (myButtonState == waitRelease)
+    {
+        Serial.println("waitRelease --> debounceRelease");
+        myButtonState = debounceRelease;
+    }
 }
