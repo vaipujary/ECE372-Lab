3bb2c70881c3234f9f401de8ea1d96c2b4eaebac
Vaidehi Pujary <vaipujary@gmail.com>
2024-04-24 15:42:55 -0700

    RFID works

--- RFID Snack Dispenser/src/main.cpp
+++ RFID Snack Dispenser/src/main.cpp
@@ -21,7 +21,7 @@
  * Signal     Pin              Pin
  *            Arduino Mega     MFRC522 board
  * -----------------------------------------------
- * Reset      46               RST
+ * Reset      49               RST
  * SPI SS     53               SDA
  * SPI MOSI   51               MOSI
  * SPI MISO   50               MISO
@@ -29,14 +29,15 @@
  */
 
 // Global variables and macros
-#define RST_PIN 9 // Configurable, see typical pin layout above
-#define SS_PIN 53 // Configurable, see typical pin layout above
+#define RST_PIN 49 // Configurable, see typical pin layout above
+#define SS_PIN 53  // Configurable, see typical pin layout above
 #define UID_LIST_SIZE 10
 
 MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
 
 int rfidUID = 0;
-int UID_list[UID_LIST_SIZE];
+int UID_list[UID_LIST_SIZE] = {609, 359};
+// Vaidehi card: 609, tag 359
 
 typedef enum
 {
@@ -58,65 +59,66 @@ int main(void)
     Serial.begin(9600); // Initialize serial port
     initSPI();          // Initialize SPI communication
     initRFID();         // Initialize RFID module
-    initSwitchPD0();    // Initialize switch
-    initTimer1();       // Initialize timer
-
-    moveCursor(0, 0); // moves the cursor to 0,0 position
-    writeString("Snack Dispenser");
-
+    // initSwitchPD0();    // Initialize switch
+    // initTimer1();       // Initialize timer
+    initTimer0();
+    // moveCursor(0, 0); // moves the cursor to 0,0 position
+    // writeString("Snack Dispenser");
     rfidUID = readRFID();
+    Serial.print("Main function The UID is: ");
+    Serial.println(rfidUID);
 
     while (1)
     {
 
         // Button state machine logic
-        switch (myButtonState)
-        {
-            ///////////////////////////////Press States/////////////////////////////////////////
-        case waitPress: // the "natural" state
-            // Do nothing, wait for button to be pressed
-            Serial.println("waitPress");
-            Serial.println(1);
-            break;
+        // switch (myButtonState)
+        // {
+        //     ///////////////////////////////Press States/////////////////////////////////////////
+        // case waitPress: // the "natural" state
+        //     // Do nothing, wait for button to be pressed
+        //     Serial.println("waitPress");
+        //     break;
 
-        case debouncePress: // Debounce Press state, wait for switch debounce state to end
-            Serial.println("debouncePress");
-            Serial.flush();
-            delayMs(1);
-            myButtonState = waitRelease;
-            break;
+        // case debouncePress: // Debounce Press state, wait for switch debounce state to end
+        //     Serial.println("debouncePress");
+        //     Serial.flush();
+        //     delayMs(1);
+        //     myButtonState = waitRelease;
+        //     break;
 
-        ///////////////////////////////Release States///////////////////////////////
-        case waitRelease: // waits for button to be released after pressed
-            Serial.println("waitRelease");
-            Serial.flush();
-            delayMs(1);
+        // ///////////////////////////////Release States///////////////////////////////
+        // case waitRelease: // waits for button to be released after pressed
+        //     Serial.println("waitRelease");
+        //     Serial.flush();
+        //     delayMs(1);
 
-            break;
+        //     break;
 
-        case debounceRelease:
-            Serial.println("debounceRelease");
-            Serial.flush();
-            delayMs(1);
-            myButtonState = waitPress;
-            break;
+        // case debounceRelease:
+        //     Serial.println("debounceRelease");
+        //     Serial.flush();
+        //     delayMs(1);
+        //     myButtonState = waitPress;
+        //     break;
 
-        default:
-            break;
-        }
+        // default:
+        //     break;
+        // }
 
         if (operationMode == normal) // Dispense snacks, turn green LEDs on, display success message on lcd
         {
             for (int i = 0; i < UID_LIST_SIZE; i++)
             {
                 // User is authorized, activate the motor
-                if (rfidUID == UID_list[i])
+                if (rfidUID == UID_list[i] && rfidUID != 0)
                 {
-                    Serial.println('RFID UID Authorized');
-                    motorCCW();       // Dispense the snacks. // Have to adjust direction of rotation based on experimentation.
-                    moveCursor(0, 0); // moves the cursor to 0,0 position
-                    writeString("Enjoy the snacks!");
-                    delayMs(5000);
+                    Serial.println("RFID UID Authorized!");
+
+                    // motorCCW();       // Dispense the snacks. // Have to adjust direction of rotation based on experimentation.
+                    // moveCursor(0, 0); // moves the cursor to 0,0 position
+                    // writeString("Enjoy the snacks!");
+                    delayMs(1000);
                 }
             }
         }
